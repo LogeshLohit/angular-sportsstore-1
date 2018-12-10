3ba26b75c75effc45cbbb9635ea4bb92da1f1056
Logesh S <logesh.ssg@gmail.com>
2018-12-10 17:58:04 +0530

    Add files via upload

---
+++ src/app/store/store.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
+@Component({
+    selector: "store",
+    templateUrl: "store.component.html"
+})
+export class StoreComponent {
+
+    public selectedCategory = null;
+
+    constructor(private repository: ProductRepository, private cart: Cart) { }
+
+    get products(): Product[] {
+        return this.repository.getProducts(this.selectedCategory);
+    }
+
+    get categories(): string[] {
+        return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
+    }
+}
